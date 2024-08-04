8e329892e018455d8490115fcb26fa21d2b8216f
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-08-04 20:22:33 +0330

    save tamrin
    page save

--- Django_Bigdely_Tamrin/urls.py
+++ Django_Bigdely_Tamrin/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from Django_Bigdely_Tamrin.views import http_test,json_test
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('http-test' ,http_test),
+    path('json-test',json_test),
 ]
