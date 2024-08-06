8ff7523d576657e3f986491af47472e563cdac4c
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-08-06 20:14:06 +0330

    created first app
    created first app named website with
    three views for contact , about and index

--- Django_Bigdely_Tamrin/views.py
+++
@@ -1,7 +0,0 @@
-from django.http import HttpResponse,JsonResponse
-
-def http_test(request):
-    return HttpResponse('http_test')
-
-def json_test(request):
-    return JsonResponse({'name': 'amir'})
