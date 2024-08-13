83d49db5796a8cb5048621a17fe87ea5ec761130
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-08-13 19:44:19 +0330

    the fixed templates
    the fixed templates in my django project

--- my_web/urls.py
+++ my_web/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from my_web.views import *
 
+app_name = 'my_web'
+
 urlpatterns = [
-    path('' ,index_view),
-    path('about',about_view),
-    path('contact',contact_view),
+    path('' ,index_view,name='index'),
+    path('about',about_view,name='about'),
+    path('contact',contact_view,name='contact'),
     ]
