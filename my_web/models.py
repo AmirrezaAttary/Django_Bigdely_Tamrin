a70f53f13e207f84998a96bc3e671487c90a7ba8
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-08-18 20:52:22 +0330

    start db in my project
    start db in my project

--- my_web/models.py
+++ my_web/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Contatct(models.Model):
+    name = models.CharField(max_length=2555)
+    email = models.EmailField()
+    subject = models.CharField(max_length=255)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
