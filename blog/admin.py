f43f4e2d846a3f1cd4116a95e92e2aaf8efebbae
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-08-25 20:10:49 +0330

    added to author_name

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ from blog.models import Post
 class PostAdmin(admin.ModelAdmin):
     date_hierarchy ='created_date'
     empty_value_display = '-empty-'
-    list_display = ('title','contend_view','status','published_date','created_date',)
-    list_filter = ('status',)
+    list_display = ('title','author','contend_view','status','published_date','created_date',)
+    list_filter = ('status','author')
     # ordering = ['-created_date']
     search_fields = ('title','contend')
 admin.site.register(Post,PostAdmin)
