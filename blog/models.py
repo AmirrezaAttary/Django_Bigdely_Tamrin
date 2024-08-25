2cf712ae8de52d1843cfa4f6c96c798965ab797f
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-08-25 23:46:38 +0330

    add media file

--- blog/models.py
+++ blog/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 # Create your models here.
 class Post(models.Model):
-    # image
+    image = models.ImageField(upload_to = 'blog/',default='blog/default.jpg')
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null=True)
     title = models.CharField(max_length=255)
     contend = models.TextField()
