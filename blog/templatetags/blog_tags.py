a1d8c2358bdbf40b306b546d8aa87d60a014166b
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-09-03 19:29:47 +0330

    fix_lastestpost

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -16,3 +16,8 @@ def func():
 @register.filter
 def snippet(value,arg = 20):
     return value[:arg] + '...'
+
+@register.inclusion_tag("blog/blog-popular-post.html")
+def latestpost(arg=3):
+    posts = Post.objects.filter(status = 1).order_by('published_date')[:arg]
+    return {'posts': posts}
