cb6a5bdcc6a861bd65d3e2e928b506c013481c5d
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-09-06 22:16:45 +0330

    add Paginator in view

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from blog.models import Post
+from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # Create your views here.
 def blog_view(request,**kwargs):
@@ -9,6 +10,14 @@ def blog_view(request,**kwargs):
         posts = posts.filter(category__name=kwargs['cat_name'])
     if kwargs.get('author_username'):
         posts = posts.filter(author__username=kwargs['author_username'])
+    posts = Paginator(posts,3)
+    try:   
+        page_number = request.GET.get('page')
+        posts = posts.get_page(page_number)
+    except PageNotAnInteger:
+        posts = posts.get_page(1)
+    except EmptyPage:
+        posts = posts.get_page(1)
     context = {'posts': posts}
     return render(request,'blog/blog-home.html',context)
 
