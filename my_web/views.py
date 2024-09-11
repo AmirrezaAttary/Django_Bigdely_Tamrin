10b2d40aa816f7a1e17b4677e01fa4e1095e3382
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-09-11 23:42:19 +0330

    django form part one

--- my_web/views.py
+++ my_web/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
-
+from my_web.models import Contatct
 def index_view(request):
     return render(request,'my_web/index.html')
 
@@ -9,4 +9,20 @@ def about_view(request):
 
 def contact_view(request):
     return render(request,'my_web/contact.html')
+
+
+def test_view(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        c = Contatct()
+        c.name = name
+        c.email = email
+        c.subject = subject
+        c.message = message
+        c.save()
+        print(name, email, subject, message)
+    return render(request,'my_web/test.html')
 
