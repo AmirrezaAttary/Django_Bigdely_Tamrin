ce5ae09e7a6ce38f20144d4db405974a9caad6b6
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-09-12 18:08:59 +0330

    django form part tree

--- my_web/forms.py
+++ my_web/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from my_web.models import Contatct
 
 class NameForm(forms.Form):
     name = forms.CharField(max_length=255)
@@ -6,3 +7,9 @@ class NameForm(forms.Form):
     subject = forms.CharField(max_length=255)
     message = forms.CharField(widget=forms.Textarea)
     
+    
+class ContatctForm(forms.ModelForm):
+
+    class Meta:
+        model = Contatct
+        fields = '__all__'
--- my_web/views.py
+++ my_web/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from my_web.models import Contatct
-from my_web.forms import NameForm
+from my_web.forms import NameForm ,ContatctForm
 def index_view(request):
     return render(request,'my_web/index.html')
 
@@ -14,18 +14,14 @@ def contact_view(request):
 
 def test_view(request):
     if request.method == 'POST':
-        form = NameForm(request.POST)
+        form = ContatctForm(request.POST)
         if form.is_valid():
-            name = form.cleaned_data['name']
-            subject = form.cleaned_data['subject']
-            email = form.cleaned_data['email']
-            message = form.cleaned_data['message']
-            print(name,subject, email, message)
+            form.save()
             return HttpResponse('done')
         else:
             return HttpResponse('error')
         
         
-    form = NameForm()
+    form = ContatctForm()
     return render(request,'my_web/test.html',{'form':form})
 
