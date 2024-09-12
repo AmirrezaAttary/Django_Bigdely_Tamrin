acf4869abaa51d2c2f0f45daf41bd96513334085
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-09-12 17:47:22 +0330

    forms part two

--- my_web/views.py
+++ my_web/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse,JsonResponse
 from my_web.models import Contatct
+from my_web.forms import NameForm
 def index_view(request):
     return render(request,'my_web/index.html')
 
@@ -13,16 +14,18 @@ def contact_view(request):
 
 def test_view(request):
     if request.method == 'POST':
-        name = request.POST.get('name')
-        email = request.POST.get('email')
-        subject = request.POST.get('subject')
-        message = request.POST.get('message')
-        c = Contatct()
-        c.name = name
-        c.email = email
-        c.subject = subject
-        c.message = message
-        c.save()
-        print(name, email, subject, message)
-    return render(request,'my_web/test.html')
+        form = NameForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            subject = form.cleaned_data['subject']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            print(name,subject, email, message)
+            return HttpResponse('done')
+        else:
+            return HttpResponse('error')
+        
+        
+    form = NameForm()
+    return render(request,'my_web/test.html',{'form':form})
 
