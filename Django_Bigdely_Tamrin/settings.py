9dd966a5f24c352ffb241f404699ff4375a0af4e
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-09-14 21:03:47 +0330

    use django-extensions

--- Django_Bigdely_Tamrin/settings.py
+++ Django_Bigdely_Tamrin/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
+    'django-extensions',
     'my_web.apps.MyWebConfig',
     'blog.apps.BlogConfig',
     
