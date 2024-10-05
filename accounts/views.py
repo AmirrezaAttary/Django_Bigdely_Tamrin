f522748f7da315496c2027549cb05a95e80a80ed
AmirrezaAttary <programmer.amirrezaattary@gmail.com>
2024-10-05 13:52:46 +0330

    fix erorr login

--- accounts/views.py
+++ accounts/views.py
@@ -15,12 +15,15 @@ def login_view(request):
         if request.method == 'POST':
             form = LoginForm(request=request,data=request.POST)
             if form.is_valid():
+                messages.add_message(request, messages.SUCCESS, 'You have successfully logged')
                 username = form.cleaned_data.get('username')
                 password = form.cleaned_data.get('password')
                 user = authenticate(request, username=username, password=password)
                 if user is not None:
                     login(request, user)
                     return redirect('/')
+            else:
+                messages.add_message(request, messages.ERROR, 'Email or username is wrong')
             
         form = LoginForm()
         context = {'form': form}
